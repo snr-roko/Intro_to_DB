358ce5889c4e5eb440ba420ffc0f4025c35d7e2c
mr-r <rbbagyei@gmail.com>
2024-07-27 10:25:16 +0000

    MySQLServer.py updated: else block now creates the database and sets it as default, commits it to the database server and prints a successful message to the console afterwards.

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,12 +4,20 @@ try:
   book_store_db = mysql.connector.connect(
   host= 'localhost',
   user= 'root',
-  passwd = '##############',
-  database = 'alx_book_store'
+  passwd = 'MrRoko@0405',
 )
 except Exception:
   print("Database could not connect, Please check your logins")
 else:
+  book_store_db_cursor = book_store_db.cursor()
+  book_store_db_cursor.execute(
+    "CREATE DATABASE IF NOT EXISTS alx_book_store"
+  )
+  book_store_db.commit()
+
+  book_store_db_cursor.execute("USE alx_book_store")
+  book_store_db.commit()
+
   print(f"Database {book_store_db.database} created successfully!")
 finally:
   book_store_db.close()
